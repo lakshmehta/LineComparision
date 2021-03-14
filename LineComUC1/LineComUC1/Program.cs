71a387eb9c6638b298fd8f8342592cdc7fd28aab
unknown <lakshmehta06@gmail.com>
2021-03-15 01:53:27 +0530

    [Laksh added]

---
+++ LineComUC1/LineComUC1/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace LineComUC1
+{
+    class Program
+    {
+
+
+
+
+        static void quadrant(int x, int y)
+
+        {
+
+            if (x > 0 && y > 0)
+
+                Console.WriteLine("lies in First quadrant");
+
+
+
+            else if (x < 0 && y > 0)
+
+                Console.WriteLine("lies in Second quadrant");
+
+
+
+            else if (x < 0 && y < 0)
+
+                Console.WriteLine("lies in Third quadrant");
+
+
+
+            else if (x > 0 && y < 0)
+
+                Console.WriteLine("lies in Fourth quadrant");
+
+
+
+            else if (x == 0 && y > 0)
+
+                Console.WriteLine("lies at positive y axis");
+
+
+
+            else if (x == 0 && y < 0)
+
+                Console.WriteLine("lies at negative y axis");
+
+
+
+            else if (y == 0 && x < 0)
+
+                Console.WriteLine("lies at negative x axis");
+
+
+
+            else if (y == 0 && x > 0)
+
+                Console.WriteLine("lies at positive x axis");
+
+
+
+            else
+
+                Console.WriteLine("lies at origin");
+
+        }
+
+
+
+  
+
+        public static void Main()
+
+        {
+
+            int x = 1, y = 1;
+
+
+
+            
+
+            quadrant(x, y);
+
+        }
+    }
+}
